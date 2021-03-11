4dbee1bce86e9447b9e0aab2e64dd822648e1b59
Oleksandr Zrodnikov <alexzrodnikov@gmail.com>
2021-03-11 17:17:38 +0200

    feat: add solution to the task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let diff;
+  
+  while (str.length != 0) {
+      for (brackets of bracketsConfig) {
+          let inter = brackets.join('');
+          str = str.replace(inter, '');
+      }
+      if (str === diff) {
+          break;
+      }
+      diff = str;
+  }
+  return str.length === 0;
 }
